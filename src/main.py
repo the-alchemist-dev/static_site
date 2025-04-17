92af5dbe2e7f3b1e9c6c3756ed818313887452de
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-04-16 22:14:38 -0500

    created exec bash script to run main file, created main.py and textnode.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from textnode import TextNode
+
+def main():
+    example_node = TextNode("This is anchor text", "link", "https://www.boot.dev")
+    print(example_node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "text"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text != other.text:
+            return False
+        if self.text_type != other.text_type:
+            return False
+        if self.url != other.url:
+            return False
+        return True
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
