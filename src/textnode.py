16053e9b6ef5273f4a591036ae2077de501fd75f
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-04-21 20:55:15 -0500

    Started writing a function to convert a text node to an html node, work in progress

--- src/textnode.py
+++ src/textnode.py
@@ -26,3 +26,11 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("invalid text type")
+
+    #need to finish this implementation
+    pass
